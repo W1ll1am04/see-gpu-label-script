d0b711b74fe1ffb51bc6f8157db4d7893b27243e
William <43065399+W1ll1am04@users.noreply.github.com>
2019-11-10 20:57:22 +0100

    --added download script.
    downloads and chmod's gpu.sh

---
+++ download.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+clear
+echo gpu.sh download script.
+echo W1ll1am github: https://github.com/W1ll1am04
+echo 
+wget https://raw.githubusercontent.com/W1ll1am04/see-vram-script/master/gpu.sh
+echo gpu.sh downloaded sucessfully.
+chmod 777 gpu.sh
+echo Finished downloading gpu.sh
+echo 
+echo you can now run it by typing: ./gpu.sh
