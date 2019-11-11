ef540a73784abd06b3a2dcceb7010092fd4e6dcb
William <43065399+W1ll1am04@users.noreply.github.com>
2019-11-11 17:53:01 +0100

    --added powerpc architecture
    --added powerpc architecture
    --for extended useablity

---
+++ gpu-alternate-powerpc.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+clear
+echo gpu.sh download script.
+echo W1ll1am github: https://github.com/W1ll1am04
+echo 
+echo Getting MESA-UTILS deb.
+wget http://ports.ubuntu.com/ubuntu-ports/pool/universe/m/mesa-demos/mesa-utils_8.1.0-2_powerpc.deb
+dpkg -i mesa-utils_8.1.0.2_powerpc.deb
+clear
+echo Installed Package MESA-UTILS
+echo 
+echo Show GPU Vram Script.
+echo Made by W1ll1am on github: https://github.com/W1ll1am04
+glxinfo | egrep -i 'device|memory'
