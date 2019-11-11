f107bbb22392ee94de8c7b2e61cb9c5e69d1ac94
William <43065399+W1ll1am04@users.noreply.github.com>
2019-11-11 18:00:53 +0100

    --added sudo for dpkg
    --added sudo for dpkg
    --user does not need to enter superuser mode anymore.

--- gpu-alternate-armhf.sh
+++ gpu-alternate-armhf.sh
@@ -6,8 +6,10 @@ echo Made by W1ll1am on github: https://github.com/W1ll1am04
 echo  
 echo Getting MESA-UTILS deb.
 sudo wget http://ports.ubuntu.com/ubuntu-ports/pool/universe/m/mesa-demos/mesa-utils_8.4.0-1_armhf.deb
-dpkg -i mesa-utils_8.4.0-1_armhf.deb
+sudo dpkg -i mesa-utils_8.4.0-1_armhf.deb
+sudo rm mesa-utils_8.4.0-1_armhf.deb
 clear
+echo Cleaning up..
 echo Installed Package MESA-UTILS.
 echo 
 echo Show GPU Vram Script.
