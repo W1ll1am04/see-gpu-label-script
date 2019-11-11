ae941a025c8729747e41dcb2574a6c7457929a1f
William <43065399+W1ll1am04@users.noreply.github.com>
2019-11-11 18:02:08 +0100

    --added sudo for dpkg
    --added sudo for dpkg
    --user does not need to enter superuser mode standalone anymore.

--- gpu-alternate-powerpc.sh
+++ gpu-alternate-powerpc.sh
@@ -5,8 +5,10 @@ echo W1ll1am github: https://github.com/W1ll1am04
 echo 
 echo Getting MESA-UTILS deb.
 wget http://ports.ubuntu.com/ubuntu-ports/pool/universe/m/mesa-demos/mesa-utils_8.1.0-2_powerpc.deb
-dpkg -i mesa-utils_8.1.0.2_powerpc.deb
+sudo dpkg -i mesa-utils_8.1.0.2_powerpc.deb
+sudo rm mesa-utils_8.1.0.2_powerpc.deb
 clear
+echo Cleaning up...
 echo Installed Package MESA-UTILS
 echo 
 echo Show GPU Vram Script.
