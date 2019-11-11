6d88f89d69830afe7855a5d84558edbda3ec4d42
William <43065399+W1ll1am04@users.noreply.github.com>
2019-11-11 10:58:10 +0100

    --added extended downloads for arm64 armhf

--- download.sh
+++ download.sh
@@ -3,7 +3,12 @@ clear
 echo gpu.sh download script.
 echo W1ll1am github: https://github.com/W1ll1am04
 echo 
+mkdir gpu-sh-script
+cd gpu-sh-script
 wget https://raw.githubusercontent.com/W1ll1am04/see-vram-script/master/gpu.sh
+wget https://raw.githubusercontent.com/W1ll1am04/see-vram-script/master/gpu-alternate-arm64.sh
+wget https://raw.githubusercontent.com/W1ll1am04/see-vram-script/master/gpu-alternate-armhf.sh
+wget https://raw.githubusercontent.com/W1ll1am04/see-vram-script/master/readme.md
 echo gpu.sh downloaded sucessfully.
 chmod 777 gpu.sh
 echo Finished downloading gpu.sh
