9be61459f29a1b36b49062364a19a515af02f338
William <43065399+W1ll1am04@users.noreply.github.com>
2019-11-11 17:57:00 +0100

    --added powerpc and chmod 777 for gpu-alternate
    --added powerpc and chmod 777 for gpu-alternate
    --for extended useability

--- download.sh
+++ download.sh
@@ -8,9 +8,13 @@ cd gpu-sh-script
 wget https://raw.githubusercontent.com/W1ll1am04/see-vram-script/master/gpu.sh
 wget https://raw.githubusercontent.com/W1ll1am04/see-vram-script/master/gpu-alternate-arm64.sh
 wget https://raw.githubusercontent.com/W1ll1am04/see-vram-script/master/gpu-alternate-armhf.sh
+wget https://raw.githubusercontent.com/W1ll1am04/see-vram-script/master/gpu-alternate-powerpc.sh
 wget https://raw.githubusercontent.com/W1ll1am04/see-vram-script/master/readme.md
-echo gpu.sh downloaded sucessfully.
+echo gpu-sh family downloaded sucessfully.
 chmod 777 gpu.sh
+chmod 777 gpu-alternate-arm64.sh
+chmod 777 gpu-alternate-armhf.sh
+chmod 777 gpu-alternate-powerpc.sh
 echo Finished downloading gpu.sh
 echo 
 echo you can now run it by typing: ./gpu.sh
