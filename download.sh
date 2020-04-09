1c0c311a20ded7276b82200d4d02271eed7bb03c
William <43065399+W1ll1am04@users.noreply.github.com>
2020-04-09 17:26:32 +0200

    Downloader fixes

--- download.sh
+++ download.sh
@@ -19,10 +19,12 @@ then
 	apt-get install $mesa --assume-yes >> /dev/null;
 fi
 
+# Checking if git is installed, if not installing it.
 if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
 	apt-get install git --assume-yes >> /dev/null;
 else
+	# Cloning into see-vram-script repository
 	git clone https://github.com/W1ll1am04/see-vram-script
 	echo -e "\n"
 	FILE=see-vram-script
