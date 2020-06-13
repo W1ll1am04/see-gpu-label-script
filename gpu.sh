368266914adf5867b4585f2b8e9bd9e71df77bda
William <43065399+W1ll1am04@users.noreply.github.com>
2020-06-14 00:53:58 +0200

    Args fix & fixed spilliting again.
    Add multiple args at the same time support, and fix formatting.

--- gpu.sh
+++ gpu.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
-# define strings & bools
 dep="lshw"
 dep2="sed"
 req_su=true
 gpu_only=false
+sdt=true
 
-function display_help() {
-  echo "usage: ./gpu.sh [arg]"
-  echo "A tool to see your gpu's name."
+function usage() {
+  echo "usage: ./gpu.sh [OPTION]... [OPTION]... [OPTION...]"
+  echo "A tool to see your gpu's label."
   echo "Arguments & usage, and an explanation: "
   echo
-  echo "-h, --help        display this message."
-  echo "-n, --no-su       dont request superuser permission."
-  echo "-g, --gpu-only    display gpu label only."
+  echo "-h, --help            display this message."
+  echo "-n, --no-su           dont request superuser permission."
+  echo "-g, --gpu-only        display gpu label only."
+  echo "-s, --skip-dep-test   skip dependency test and just run the program."
+}
+
+function dependency_test() {
+  # check if 'lshw' is installed, if it isnt install it. Because the whole script relies on 'lshw' to get gpu information.
+  if [ $(dpkg-query -W -f='${Status}' $dep 2>/dev/null | grep -c "ok installed") -eq 0 ];
+  then
+    echo "Installing $dep"
+    if sudo apt-get install $dep >> /dev/null;
+    then
+      echo "Done installing $dep"
+      echo
+    else
+      echo "Failed installing $dep, you will have to manually install $dep."
+      echo
+    fi
+  fi 
+
+  # check if 'sed' is installed, if it isnt install it. Because the splitting relies on 'sed'.
+  if [ $(dpkg-query -W -f='${Status}' $dep2 2>/dev/null | grep -c "ok installed") -eq 0 ];
+  then
+    echo "Installing $dep2"
+    if sudo apt-get install $dep2 >> /dev/null;
+    then
+      echo "Done installing $dep2"
+      echo
+    else
+      echo "Failed installing $dep2, you will have to manually install $dep."
+      echo
+    fi
+  fi 
 }
 
 # parse arguments
@@ -21,60 +52,46 @@ if [ $# -eq 0 ]
 then
   true
 else
-  if [ "$1" == "--help" ] || [ "$1" == "-h" ]
-  then
-    display_help
-    exit
-  # yes yes i know i should do two args at once. i will eventually do it (:
-  else if [ "$1" == "--no-su" ] || [ "$1" == "-n" ]
-  then
-    req_su=false
-  else if [ "$1" == "--gpu-only" ] || [ "$1" == "-g" ]
-  then
-    gpu_only=true
-  else
-    echo "Unknown parameter: '$1'"
-    echo "Use -h for more information."
-    exit
-  fi
-  fi
-  fi
+  for arg in "$@"; do
+    shift
+    case "$arg" in
+      "--help") set -- "$@" "-h" ;;
+      "--no-su") set -- "$@" "-n" ;;
+      "--gpu-only")   set -- "$@" "-g" ;;
+      "--skip-dep-test")   set -- "$@" "-s" ;;
+      *)        set -- "$@" "$arg"
+    esac
+  done
+
+  while getopts "hngs" options; do
+    case "${options}" in
+      h)
+        usage
+        exit
+        ;;
+      n)
+        req_su=false
+        ;;
+      g)
+        gpu_only=true
+        ;;
+      s)
+        sdt=false
+        ;;
+    esac
+  done
 fi
 
 function credit() {
   echo "see-vram-script."
   echo "https://github.com/W1ll1am04"
   echo ""
-  return 0;
 }
 
-# check if 'lshw' is installed, if it isnt install it. Because the whole script relies on 'lshw'.
-if [ $(dpkg-query -W -f='${Status}' $dep 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ "$sdt" = true ]
 then
-  echo "Installing $dep"
-  if sudo apt-get install $dep >> /dev/null;
-  then
-    echo "Done installing $dep"
-    echo
-  else
-    echo "Failed installing $dep, you will have to manually install $dep."
-    echo
-  fi
-fi 
-
-# check if 'sed' is installed, if it isnt install it. Because the splitting relies on 'sed'.
-if [ $(dpkg-query -W -f='${Status}' $dep2 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  echo "Installing $dep2"
-  if sudo apt-get install $dep2 >> /dev/null;
-  then
-    echo "Done installing $dep2"
-    echo
-  else
-    echo "Failed installing $dep2, you will have to manually install $dep."
-    echo
-  fi
-fi 
+  dependency_test
+fi
 
 if [ "$gpu_only" = false ] ; 
 then
@@ -94,16 +111,19 @@ fi
 readarray -d "y" -t strarr <<< "$gpu_command"
 gpu=`echo ${strarr[1]} | sed 's/ *$//g'`
 
-# readarray -d "" -t gpu <<< "${almost[1]}"
-if [ "$chip_only" = true ] ; 
+if [ "$gpu_only" = true ] ; 
 then
-  split_space=(${almost[0]})
-  echo "${split_space[0]}"
-else
-  if [ "$gpu_only" = true ] ; 
+  if [ -z "$gpu" ]
   then
+    echo "Unable to get GPU label."
+  else
     echo -e "$gpu"
+  fi
+else
+  if [ -z "$gpu" ]
+  then
+    echo "Unable to get GPU label."
   else
     echo -e "GPU: $gpu"
-fi
+  fi
 fi
